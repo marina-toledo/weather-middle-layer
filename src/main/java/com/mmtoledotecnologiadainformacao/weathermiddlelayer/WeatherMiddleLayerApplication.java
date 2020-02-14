9378e9bd8b79404aaadec17a635dd264147060e5
marina <marina.mi.toledo@hotmail.com>
2020-02-14 16:09:42 -0300

    Using Spring Initializr to create the base of this project. (https://start.spring.io)

---
+++ src/main/java/com/mmtoledotecnologiadainformacao/weathermiddlelayer/WeatherMiddleLayerApplication.java
@@ -0,0 +1,13 @@
+package com.mmtoledotecnologiadainformacao.weathermiddlelayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMiddleLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMiddleLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmtoledotecnologiadainformacao/weathermiddlelayer/WeatherMiddleLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmtoledotecnologiadainformacao.weathermiddlelayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMiddleLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
